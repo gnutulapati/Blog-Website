7e997be3c9daf5b8c17129f10d2a32601a034ab8
Gourav Nutulapati <157598386+gnutulapati@users.noreply.github.com>
2024-09-10 16:22:31 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,20 +1,27 @@
-const express = require('express')
-const articleRouter = require("./routes/articles")
-const Article = require('./models/article')
-const mongoose = require('mongoose')
-const methodOverride = require('method-override')
-const app = express()
+const express = require("express");
+const articleRouter = require("./routes/articles");
+const Article = require("./models/article");
+const mongoose = require("mongoose");
+const methodOverride = require("method-override");
+const app = express();
 
-mongoose.connect('mongodb://localhost/bharatInternDatabase')
-app.set("views", "./view")
-app.set('view engine', 'ejs')
-app.use(express.urlencoded({extended: false}))
-app.use(methodOverride('_method'))
-app.get('/', async(req, res) => {
-    const articles =await Article.find().sort({ createdAt:'desc'})
-    res.render('articles/index', { articles: articles })
-})
+mongoose.connect(
+  `mongodb+srv://${username}:${password}@cluster0.psuqk.mongodb.net/registrationFormDB`,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
 
-app.use('/articles', articleRouter)
+app.set("views", "./view");
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride("_method"));
+app.get("/", async (req, res) => {
+  const articles = await Article.find().sort({ createdAt: "desc" });
+  res.render("articles/index", { articles: articles });
+});
 
-app.listen(3000)
+app.use("/articles", articleRouter);
+
+app.listen(3000);
